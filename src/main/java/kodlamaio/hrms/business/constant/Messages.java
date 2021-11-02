4466b530a787e5827d87ca0fcdfb987abfb3a083
Nur Yuce <79469109+YuceNurs@users.noreply.github.com>
2021-11-02 17:59:07 +0300

    Add files via upload

---
+++ src/main/java/kodlamaio/hrms/business/constant/Messages.java
@@ -0,0 +1,37 @@
+package hrms.demo.business.constant;
+
+public class Messages {
+	public static String userListed ="Kullanıcılar listelendi ";
+	public static String userAdded ="Kullanıcı eklendi.";
+	public static String userDeleted ="Kullanıcı silindi";
+	public static String userUpdate ="Kullanıcı güncellendi.";
+	
+	public static String notNull ="Alanlar boş bırakılamaz.";
+	public static String emailExist ="Bu email daha öncesinde kullanılmış";
+	public static String passwordNotComfirmed ="Parola hatası.";
+	
+	public static String personInvalid ="Doğrulama hatası";
+	public static String nationalIdendityExist ="Kimlik doğrulaması hatası.";
+	public static String nationalIdentityInvalid ="Kimlik numarınız 11 haneli olmalıdır.";
+	
+	public static String jobTitleExist ="İş ismi hazırda mevcut";
+	public static String jobPositionAdded ="İş Pozisyonu eklendi.";
+	public static String jobPositionListed ="İş Pozisyonları listelendi.";
+	
+	public static String EmployeeAdded ="Çalışan eklendi.";
+	public static String jobPostAdded ="İş ilanı eklendi.";
+	public static String jobPostUpdate ="İlan  güncellendi.";
+	
+	public static String activationCodeAdded = "Aktivasyon kodu eklendi.";
+    public static String activationCodeDeleted = "Aktivasyon kodu silindi.";
+    public static String activationCodeUpdated = "Aktivasyon kodu güncellendi.";
+    public static String activationCodeListed = "Aktivasyon kodları listelendi.";
+    public static String codeNotFound = "Aktivasyon kodu bulunamadı";
+    public static String activationExist = "Bu email zaten doğrulanmış";
+    public static String codeExpired = "Aktivasyon kodunun zamanı geçmiş, bir zahmet yenisini iste";
+    public static String codeNotEqual = "Aktivasyon kodu geçersiz";
+    public static String codeVerified = "Aktivasyon kod doğrulaması başarılı";
+	
+	
+
+}
